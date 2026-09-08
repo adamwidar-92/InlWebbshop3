f14189b8197fc79d4fc358440148c894fc6cfe32
G <George.l1@hotmail.com>
2026-09-08 19:39:12 +0200

    Skapa grund för leveransformulär #14

--- web/src/pages/Checkout.tsx
+++ web/src/pages/Checkout.tsx
@@ -1,13 +1,21 @@
-import { Container, Box, Typography } from '@mui/material'
+import { Box, Container, TextField, Typography } from '@mui/material'
 
 export default function Checkout() {
   return (
     <Container sx={{ py: 8 }}>
-      <Box>
+      <Box component="form">
         <Typography variant="h3" component="h1" gutterBottom>
           Kassa
         </Typography>
+
+        <TextField
+          fullWidth
+          label="Namn"
+          name="customerName"
+          autoComplete="name"
+        />
       </Box>
     </Container>
   )
 }
+
