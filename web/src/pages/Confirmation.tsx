89bdf50349d2fa6cf47e45fcd10308e30eb711a6
G <George.l1@hotmail.com>
2026-09-13 16:17:47 +0200

    Visa ordernummer på bekräftelsesidan #21

--- web/src/pages/Confirmation.tsx
+++ web/src/pages/Confirmation.tsx
@@ -1,11 +1,15 @@
-import { Container, Box, Typography } from '@mui/material'
-
+import { Box, Container, Typography } from '@mui/material'
+import { useParams } from 'react-router-dom'
 export default function Confirmation() {
+  const { orderNumber } = useParams()
   return (
     <Container sx={{ py: 8 }}>
       <Box>
         <Typography variant="h3" component="h1" gutterBottom>
           Bekräftelse
+        </Typography>
+        <Typography>
+          Ordernummer: {orderNumber}
         </Typography>
       </Box>
     </Container>
